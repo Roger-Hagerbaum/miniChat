4be09a33acc9ee122f0c9528a211d53cc4b0cb7d
Roger Hagerbaum <roger.hagerbaum@lerniastudent.se>
2023-03-03 15:02:43 +0100

    created basic client and run simpel server test

--- chatApp/src/client.java
+++ chatApp/src/client.java
@@ -1,4 +1,23 @@
 
-public class client {
-    
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+
+class client {
+
+    public void runClient() throws IOException {
+        try (Socket socket = new Socket("localhost", 88)) {
+            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+
+               dataOutputStream.writeUTF("Testing if message is delivered");
+        }
+
+           
+
+
+    }
+
 }
---
+++ chatApp/src/serverTest.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+public class serverTest {
+    public static void main(String[] args) throws IOException {
+        client client = new client();
+
+        client.runClient();
+       
+
+    }
+
+}
