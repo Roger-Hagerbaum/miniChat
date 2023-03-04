b1dd799394c1d60a1e638dd9723cb04a49c618a1
Roger Hagerbaum <roger.hagerbaum@lerniastudent.se>
2023-03-04 18:56:52 +0100

    add parameter to client

--- chatApp/src/client.java
+++ chatApp/src/client.java
@@ -7,11 +7,11 @@ import java.net.Socket;
 
 class client {
 
-    public void runClient() throws IOException {
-        try (Socket socket = new Socket("localhost", 88)) {
+    public void runClient(int port , String user) throws IOException {
+        try (Socket socket = new Socket("localhost", port)) {
             DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
 
-
+                // TODO implement username
                dataOutputStream.writeUTF("Testing if message is delivered");
         }
 
