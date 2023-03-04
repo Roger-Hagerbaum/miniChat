03e308407a06e6a5cd588a1e7db30fcbc189e44d
Roger Hagerbaum <roger.hagerbaum@lerniastudent.se>
2023-03-04 22:43:59 +0100

    Add runnable thread to make server run separately

--- chatApp/src/server.java
+++ chatApp/src/server.java
@@ -8,31 +8,33 @@ import java.net.Socket;
 
  public class server {
 
-     public void startServer (int port) throws IOException{
-        
-         try (ServerSocket serverSocket = new ServerSocket(port)) {
+    public void startServer (int port) throws IOException{
+        Thread ServerThread1 = new Thread(() -> {
+            
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
             Socket socket = serverSocket.accept();
 
-                 InputStream inputStream = socket.getInputStream();
-                 DataInputStream dataInputStream = new DataInputStream(inputStream);
-                 
-                 String message;
-                // String userName; TODO implement varibel
-                // String JWT; TODO implement varibel
-                // String messageResived; TODO implement varibel
-                 while (true){
-                 try {
-                     message = dataInputStream.readUTF();
-                 } catch (IOException e) {
-                     throw new RuntimeException(e);
-                 }
+            InputStream inputStream = socket.getInputStream();
+            DataInputStream dataInputStream = new DataInputStream(inputStream);
+
+            String message;
+            // String userName; TODO implement varibel
+            // String JWT; TODO implement varibel
+            // String messageResived; TODO implement varibel
+            while (true){
+                try {
+                    message = dataInputStream.readUTF();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
 
                 System.out.println("Message Resived: " + message);
-             }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-         }
-         
-     
-     
- }
+        });
+        ServerThread1.start();
+    }
+    }
  
