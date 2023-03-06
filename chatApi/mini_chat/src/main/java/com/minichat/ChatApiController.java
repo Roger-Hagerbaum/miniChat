73800f7558fdeb344c2c745bdc0c1aecb18815ee
Roger Hagerbaum <roger.hagerbaum@lerniastudent.se>
2023-03-06 23:57:13 +0100

    Start setup on controller

--- chatApi/mini_chat/src/main/java/com/minichat/ChatApiController.java
+++ chatApi/mini_chat/src/main/java/com/minichat/ChatApiController.java
@@ -1,5 +1,21 @@
 package com.minichat;
 
-public class ChatApiController {
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class ChatApiController {
+    UserRepository userRepository;
+
+    public ChatApiController(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+
+     @PostMapping("/user")
+     public Object getUser() {
+        
+         return null ;
+     }
+  
 }
